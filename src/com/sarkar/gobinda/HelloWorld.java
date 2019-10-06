691497f0039242d2bd7fd1d9924959bc5d56aace
Gobinda Sarkar <sarkar.gobinda@gmail.com>
2019-10-06 11:35:04 -0400

    Helloword Project for the Jenkins fipeline

---
+++ src/com/sarkar/gobinda/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.sarkar.gobinda;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("This is just sample project to test Jenkins pipeline.");
+
+	}
+
+}
